a5517702f0b8c05862fcf3ba8ce01bbe23c9d36d
Yaroslav <superbobs777@gmail.com>
2022-10-08 21:55:53 +0300

    Customized templates

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "homepage"
+
 urlpatterns = [
     path("", views.greeting_for_user, name="index"),
     path("<str:username>", views.greeting_for_user, name="index"),
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 from apps.base.services.random_name_generation import generate_random_name
 
 
-def greeting_for_user(request: HttpRequest, username: str | None = None) -> HttpResponse:
+def greeting_for_user(
+    request: HttpRequest, username: str | None = None
+) -> HttpResponse:
     if username is None:
         username = generate_random_name()
 
-    return HttpResponse(f"<h2>Hello, {username}. Welcome to Django homework!</h2>")
+    return render(
+        request,
+        "base.html",
+        {
+            "homepage": f"Hello, {username}! Welcome to Django homework!"
+        }
+    )
