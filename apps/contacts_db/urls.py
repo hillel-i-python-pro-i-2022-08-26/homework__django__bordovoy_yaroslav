0035fb6588ab5585a8e254f771f825418486e854
Yaroslav <superbobs777@gmail.com>
2022-11-09 20:22:10 +0200

    Add CRUD

--- apps/contacts_db/urls.py
+++ apps/contacts_db/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = "contacts_db"
 
 urlpatterns = [
-    path("", views.contacts_view, name="index"),
+    path("", views.show_all, name="index"),
+    path("create-user/", views.create_contact, name="create_contact"),
+    path("search-contact/", views.search_contact, name="search_contact"),
+    path("read-contact/", views.read_contact, name="read_contact"),
+    path("delete-contact/<int:pk>/", views.delete_contact, name="delete_contact"),
+    path("update-contact/<int:pk>/", views.update_contact, name="update_contact"),
+    path("contact/<int:pk>/", views.contact, name="contact"),
 ]
--- apps/contacts_db/views.py
+++ apps/contacts_db/views.py
@@ -1,16 +1,101 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import ContactsForm
 from .models import Contacts
 
 
-def contacts_view(request: HttpRequest) -> HttpResponse:
+def show_all(request: HttpRequest) -> HttpResponse:
     contacts = Contacts.objects.all()
     return render(
         request,
-        "templates_for__contacts_db/content.html",
+        "templates_for__contacts_db/base_content.html",
         {
             "title": "Contacts",
-            "contacts_db": contacts,
+            "contacts": contacts,
         },
     )
+
+
+def contact(request: HttpRequest, pk: Contacts.pk) -> HttpResponse:
+    contact = get_object_or_404(Contacts, pk=pk)
+    return render(
+        request,
+        "templates_for__contacts_db/contact.html",
+        {
+            "title": "Contact information",
+            "contact": contact,
+        },
+    )
+
+
+def search_contact(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        "templates_for__contacts_db/search_contact.html",
+        {
+            "title": "Search contact",
+        },
+    )
+
+
+def create_contact(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ContactsForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            contact.save()
+            return redirect("contacts_db:contact", pk=contact.pk)
+    else:
+        form = ContactsForm()
+    return render(
+        request,
+        "templates_for__contacts_db/create_contact.html",
+        {
+            "title": "Create contact",
+            "form": form,
+        },
+    )
+
+
+
+def read_contact(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get("q")
+
+    if query.startswith("0"):
+        contact = Contacts.objects.get(phone=query)
+    else:
+        contact = Contacts.objects.get(pk=query)
+    return render(
+        request,
+        "templates_for__contacts_db/read_contact.html",
+        {
+            "title": "Contact information",
+            "contact": contact,
+        },
+    )
+
+
+def update_contact(request: HttpRequest, pk: Contacts.pk) -> HttpResponse:
+    contact = get_object_or_404(Contacts, pk=pk)
+    if request.method == "POST":
+        form = ContactsForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect("contacts_db:contact", pk=pk)
+        else:
+            form = ContactsForm(instance=contact)
+    return render(
+            request,
+            "templates_for__contacts_db/update_contact.html",
+            {
+                "title": "Update contact",
+                "form": form,
+            },
+        )
+
+
+def delete_contact(request: HttpRequest, pk: Contacts.pk) -> HttpResponse:
+    contact = get_object_or_404(Contacts, pk=pk)
+    contact.delete()
+    return redirect("contacts_db:index")
