2f495b5998e035a89f0c7282e52578ca40e18c9e
Yaroslav <superbobs777@gmail.com>
2022-12-11 23:15:18 +0200

    Login required

--- apps/sessions_app/urls.py
+++ apps/sessions_app/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
@@ -5,5 +6,5 @@ from . import views
 app_name = "sessions_app"
 
 urlpatterns = [
-    path("", views.sessions_app, name="index"),
+    path("", login_required(views.sessions_app), name="index"),
 ]
